1a45695319e864b565c4ec61fb868bb2c66c49b7
ielanta <vanda2@mail.ru>
2017-04-25 23:30:48 +0300

    Add contacts page

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^contacts/$', views.contacts, name='contacts'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,11 @@
+from django.conf import settings
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def contacts(request):
+    context = {'mail_service': settings.MAIL_SERVICE, 'mail_marketing': settings.MAIL_MARKETING}
+    return render(request, 'contacts.html', context)
