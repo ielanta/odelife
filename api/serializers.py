f4e3c90cbe7ec3a6a658bb9126f6289ed925afb8
ielanta <vanda2@mail.ru>
2017-05-11 03:52:10 +0300

    Add working search with AJAX

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from aroma.models import Aroma
+from core.settings import GENDER_CHOICES
 
 
 class AromaListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Aroma
-        fields = ('id', 'title', 'year', 'brand')
+        fields = ('id', 'title', 'year', 'gender')
+
+    year = serializers.IntegerField(label="Год", required=False)
+    title = serializers.CharField(label="Название", required=False)
+    gender = serializers.ChoiceField(choices=GENDER_CHOICES, label="Пол", required=False)
+
--- api/views.py
+++ api/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 
 from rest_framework import generics
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
 from .serializers import AromaListSerializer
 from aroma.models import Aroma
 
 
 class AromaList(generics.ListCreateAPIView):
-    queryset = Aroma.objects.filter(is_public = True)
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'search.html'
     serializer_class = AromaListSerializer
+    allowed_methods = ['GET']
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('gender', 'year')
+    queryset = Aroma.objects.filter(is_public=True)
+
+    def list(self, request, *args, **kwargs):
+        response = super(AromaList, self).list(request, format='json', *args, **kwargs)
+        serializer = AromaListSerializer(data=request.query_params)
+        if not serializer.is_valid():
+            serializer = AromaListSerializer()
+        return Response({'items': response.data['results'], 'serializer': serializer})
