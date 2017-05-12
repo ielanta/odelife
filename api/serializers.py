37952cf68b17885d4157583c9c231979e3d0070c
ielanta <vanda2@mail.ru>
2017-05-12 18:41:30 +0300

    Use search form for better rendering

--- api/serializers.py
+++ api/serializers.py
@@ -1,19 +1,34 @@
-from django.forms.widgets import CheckboxSelectMultiple, RadioSelect
-from django.forms import widgets
 from django import forms
+from django_filters.rest_framework import NumberFilter, FilterSet, MultipleChoiceFilter, CharFilter
 from rest_framework import serializers
 from aroma.models import Aroma
 from core.settings import GENDER_CHOICES
 
 
-class AromaListSerializer(serializers.ModelSerializer):
+class SearchFilter(FilterSet):
+    min_year = NumberFilter(name="year", lookup_expr='gte')
+    max_year = NumberFilter(name="year", lookup_expr='lte')
+    title = CharFilter(name="title", lookup_expr='contains')
+    gender = MultipleChoiceFilter(choices=GENDER_CHOICES,
+                                  method=lambda queryset, name, value: queryset.filter(gender__in=value))
+
+    class Meta:
+        model = Aroma
+        fields = ['gender', 'min_year', 'max_year', 'title']
+
+
+class AromaSearchForm(forms.Form):
     class Meta:
         model = Aroma
         fields = ('id', 'title', 'gender', 'min_year', 'max_year', 'brand')
+    title = forms.CharField(label="Название", required=False)
+    gender = forms.MultipleChoiceField(choices=GENDER_CHOICES, label="Пол", required=False,
+                                       widget=forms.CheckboxSelectMultiple)
+    min_year = forms.IntegerField(label="Год(c)", required=False)
+    max_year = forms.IntegerField(label="Год(по)", required=False)
 
-    min_year = serializers.IntegerField(label="Год(c)", required=False, default=None)
-    max_year = serializers.IntegerField(label="Год(по)", required=False, default=None)
-    title = serializers.CharField(label="Название", required=False)
-    gender = serializers.MultipleChoiceField(choices=GENDER_CHOICES, label="Пол", required=False)
-    #brand = serializers.CharField(label="Дизайнер")
 
+class AromaListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aroma
+        fields = ('id', 'title', 'gender', 'year', 'brand')
--- api/views.py
+++ api/views.py
@@ -1,22 +1,9 @@
-from django_filters.rest_framework import DjangoFilterBackend, NumberFilter, FilterSet, MultipleChoiceFilter, CharFilter
-from core.settings import GENDER_CHOICES
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
-from .serializers import AromaListSerializer
+from .serializers import AromaListSerializer, SearchFilter, AromaSearchForm
 from aroma.models import Aroma
-
-
-class SearchFilter(FilterSet):
-    min_year = NumberFilter(name="year", lookup_expr='gte')
-    max_year = NumberFilter(name="year", lookup_expr='lte')
-    title = CharFilter(name="title", lookup_expr='contains')
-    gender = MultipleChoiceFilter(choices=GENDER_CHOICES,
-                                  method=lambda queryset, name, value: queryset.filter(gender__in=value))
-
-    class Meta:
-        model = Aroma
-        fields = ['gender', 'min_year', 'max_year', 'title']
 
 
 class AromaList(generics.ListCreateAPIView):
@@ -30,7 +17,5 @@ class AromaList(generics.ListCreateAPIView):
 
     def list(self, request, *args, **kwargs):
         response = super(AromaList, self).list(request, format='json', *args, **kwargs)
-        serializer = AromaListSerializer(data=request.query_params)
-        if not serializer.is_valid():
-            serializer = AromaListSerializer()
-        return Response({'items': response.data['results'], 'serializer': serializer})
+        form = AromaSearchForm(data=request.query_params)
+        return Response({'items': response.data['results'], 'form': form})
