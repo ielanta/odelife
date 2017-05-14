9d9611fa407028b1addbfa88ee478ca95b8a79ed
ielanta <vanda2@mail.ru>
2017-05-14 17:52:43 +0300

    Refactoring

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import AromaList
 
-urlpatterns = patterns(
-    url(r'^$', 'api_root'),
+urlpatterns = [
     url(r'^aromas/$', AromaList.as_view(), name='aroma-list'),
-)
+]
 
 # Format suffixes
 urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'api'])
