30b717fccfcdfdeca413d63cedb14207d4f9900c
ielanta <vanda2@mail.ru>
2017-05-30 20:18:58 +0300

    Update search results data

--- aroma/templatetags/aroma_extras.py
+++ aroma/templatetags/aroma_extras.py
@@ -1,29 +1,26 @@
 from django import template
 from django.template import TemplateSyntaxError
-from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
 
 @register.filter
 def notes_in_category(notes, category):
-    print(notes, category)
     return notes.filter(categorynotes__category=category)
 
 
 @register.filter
 def notes_in_category_exists(notes, category):
-    print(notes, category)
     return notes.filter(categorynotes__category=category).exists()
 
 
-@register.filter(is_safe=False)
-@stringfilter
+@register.filter
 # {{ val|ru_pluralize: 'аромат', 'аромата', 'ароматов' }}
-def ru_pluralize(n, str1, str2, str3):
+def ru_pluralize(n, args):
+    str1, str2, str3 = args.split(',')
     try:
-        return str1 if n % 10 == 1 & n % 100 != 11 else str2 if n % 10 >= 2 & n % 10 <= 4 & (
-            n % 100 < 10 | n % 100 >= 20) else str3
+        return str1 if (n % 10 == 1) & (n % 100 != 11) else str2 if (n % 10 >= 2) & (n % 10 <= 4) & (
+            (n % 100 < 10) | (n % 100 >= 20)) else str3
     except:
         raise TemplateSyntaxError
 
