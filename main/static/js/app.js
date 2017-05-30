31ee108b4873124392b6c3af3beab42b8d009c1e
ielanta <vanda2@mail.ru>
2017-05-31 00:40:06 +0300

    Fix top navigation + refactoring

---
+++ main/static/js/app.js
@@ -0,0 +1,23 @@
+
+$(function() {
+    function onNavbar() {
+        if (window.innerWidth >= 768) {
+            $('.navbar-custom .dropdown').on('mouseover', function(){
+                $('.dropdown-toggle', this).next('.dropdown-menu').show();
+            }).on('mouseout', function(){
+                $('.dropdown-toggle', this).next('.dropdown-menu').hide();
+            });
+            $('.dropdown-toggle').click(function() {
+                if ($(this).next('.dropdown-menu').is(':visible')) {
+                    window.location = $(this).attr('href');
+                }
+            });
+        } else {
+            $('.navbar-default .dropdown').off('mouseover').off('mouseout');
+        }
+    }
+    $(window).resize(function() {
+        onNavbar();
+    });
+    onNavbar();
+});
