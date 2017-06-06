8cd6822e2d77dcd5c9c713e09e74e94b0fd05c27
ielanta <vanda2@mail.ru>
2017-06-06 17:36:19 +0300

    Add error handlers

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib import admin
+# from django.views.defaults import page_not_found, server_error
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^aromas/', include('aroma.urls')),
     url(r'^', include('main.urls')),
+    # url(r'^404/$', page_not_found, kwargs={'exception': Exception("Page not Found")}),
+    # url(r'^500/$', server_error),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
