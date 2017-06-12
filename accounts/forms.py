5790196c01ae64608a1e0d473d204b61478768d5
ielanta <vanda2@mail.ru>
2017-06-12 23:27:58 +0300

    Add email template

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,15 +1,14 @@
-from django import forms
-from django.core.urlresolvers import reverse
-
 from registration.forms import RegistrationFormUniqueEmail
 
 
 class ExtRegistrationForm(RegistrationFormUniqueEmail):
+
     def __init__(self, *args, **kwargs):
         super(ExtRegistrationForm, self).__init__(*args, **kwargs)
         self.fields['username'].help_text = ''
         self.fields['password2'].label = 'Повторите пароль, чтобы не ошибиться'
         self.fields['password2'].help_text = ''
+
 
     # def __init__(self, *args, **kwargs):
     #     from crispy_forms.helper import FormHelper
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
 
-# Create your views here.
+from registration.backends.simple.views import RegistrationView
+
+
+class ExtRegistrationView(SuccessMessageMixin, RegistrationView):
+    success_message = 'Письмо со ссылкой для активации было успешно отправлено'
+
+    def get_success_url(self, user):
+        return reverse_lazy('registration_register')
