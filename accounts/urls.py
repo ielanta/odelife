f55c64166670a6bb738401b3a389bd0346a75871
ielanta <vanda2@mail.ru>
2017-06-13 18:21:48 +0300

    Add resend activation

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url, include
 from accounts.forms import ExtRegistrationForm
-from accounts.views import ExtRegistrationView
+from accounts.views import ExtRegistrationView, ExtActivationView, ExtResendActivationView, ExtPasswordResetView
 
 urlpatterns = [
     url(r'^register/$', ExtRegistrationView.as_view(form_class=ExtRegistrationForm), name='registration_register'),
-    url('', include('registration.backends.default.urls')),
+    url(r'^activate/resend/$', ExtResendActivationView.as_view(), name='registration_resend_activation'),
+    url(r'^activate/(?P<activation_key>\w+)/$', ExtActivationView.as_view(), name='registration_activate'),
+    url(r'^password/reset/$', ExtPasswordResetView.as_view(), name='auth_password_reset'),
+    url(r'^', include('registration.backends.default.urls')),
 ]
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,30 @@
+from django.shortcuts import redirect
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.views import PasswordResetView
 
-from registration.backends.simple.views import RegistrationView
+
+from registration.backends.default.views import RegistrationView, ActivationView, ResendActivationView
 
 
 class ExtRegistrationView(SuccessMessageMixin, RegistrationView):
     success_message = 'Письмо со ссылкой для активации было успешно отправлено'
+    success_url = 'registration_register'
 
+
+class ExtActivationView(ActivationView):
     def get_success_url(self, user):
-        return reverse_lazy('registration_register')
+        return reverse_lazy('aroma-list')
+
+
+class ExtResendActivationView(SuccessMessageMixin, ResendActivationView):
+    success_message = 'Письмо со ссылкой для активации было успешно отправлено'
+
+    def render_form_submitted_template(self, form):
+        return redirect(reverse_lazy('registration_resend_activation'))
+
+
+class ExtPasswordResetView(SuccessMessageMixin, PasswordResetView):
+    success_message = 'Письмо со ссылкой для смены пароля было успешно отправлено'
+    success_url = reverse_lazy('auth_password_reset')
+    html_email_template_name = 'registration/password_reset_email.html'
