d3a38f034d9b17cb9e645fa706a38c17840d02a1
ielanta <vanda2@mail.ru>
2017-06-15 20:44:22 +0300

    Add Fb and Vk authorization

---
+++ accounts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+
+
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='account')
+
+    @receiver(post_save, sender=User)
+    def save_user_account(sender, instance, **kwargs):
+        if not hasattr(instance, 'account'):
+            Account.objects.create(user=instance)
+
+    def get_full_name(self):
+        return self.user.get_full_name() or self.user.username
+
+    def get_short_name(self):
+        return self.user.get_short_name() or self.user.username
--- core/settings.py
+++ core/settings.py
@@ -42,7 +42,6 @@ MIDDLEWARE_CLASSES = [
 ]
 
 ROOT_URLCONF = 'core.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -167,6 +166,18 @@ AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
     'social_core.backends.facebook.FacebookOAuth2',
     'social_core.backends.vk.VKOAuth2',
+)
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
 )
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
