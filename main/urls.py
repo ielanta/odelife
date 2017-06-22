d3717a5aab4e7307e411876d3159183e7a81b804
ielanta <vanda2@mail.ru>
2017-06-22 18:33:08 +0300

    Add contact form

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from . import views
 from aroma.views import AromaCompactSearch
+from main.views import ContactView
 
 urlpatterns = [
     url(r'^$', AromaCompactSearch.as_view(), name='home'),
-    url(r'^contacts/$', views.contacts, name='contacts'),
+    url(r'^contacts/$', ContactView.as_view(), name='contacts'),
     url(r'^tos/$', views.tos, name='tos'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,27 @@
-from django.conf import settings
+from django.core.mail import send_mail
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+from django.core.urlresolvers import reverse_lazy
+from django.template import loader
+
+from main.forms import ContactForm
 
 
 def tos(request):
     return render(request, 'tos.html')
 
 
-def contacts(request):
-    context = {'mail_service': settings.MAIL_SERVICE, 'mail_marketing': settings.MAIL_MARKETING}
-    return render(request, 'contacts.html', context)
+class ContactView(SuccessMessageMixin, FormView):
+    form_class = ContactForm
+    template_name = 'contact_form/contact_form.html'
+    success_message = 'Ваше обращение было успешно отправлено'
+    success_url = reverse_lazy('contacts')
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(data=request.POST)
+        if form.is_valid():
+            html_message = loader.render_to_string(form.html_email_template_name, self.get_context_data())
+            send_mail(form.subject, request.POST.get('msg'), form.from_email, [form.to_email],
+                      html_message=html_message)
+        return super(ContactView, self).post(request, *args, **kwargs)
