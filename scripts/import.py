3042e6d38c5ccd09d8aec926241cf6507f527010
ielanta <vanda2@mail.ru>
2017-07-04 20:29:28 +0300

    Remove favicon from cashed static

--- scripts/import.py
+++ scripts/import.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 
-import ast
-import csv
-from aroma.models import Aroma, Brand, Note, Nose, AromaCounter, CategoryNotes, Group
 
+def group_import_csv(name='/Users/marinapolyakova/PycharmProjects/odelife/csv_groups/цветочные фруктовые сладкие.csv'):
+    import csv
+    import ast
+    from aroma.models import Aroma, Brand, Note, Nose, AromaCounter, CategoryNotes, Group
 
-def group_import_csv(name='/Users/marinapolyakova/PycharmProjects/fragrantica_parser/фужерные водяные.csv'):
+    def note_parser(notes, category):
+        for note in ast.literal_eval(notes):
+            note = note.strip()
+            note, _ = Note.objects.get_or_create(title=note)
+            note.save()
+            cnote, _ = CategoryNotes.objects.get_or_create(aroma=aroma, note=note, category=category)
+            cnote.save()
+
     print('here!')
     keys = (['title', 'description', 'gender', 'designer', 'pic', 'noses', 'year', 'Ноты Аромата', 'верхние Ноты',
              'средние Ноты', 'база Ноты', 'comments'])
@@ -13,11 +21,15 @@ def group_import_csv(name='/Users/marinapolyakova/PycharmProjects/fragrantica_pa
         reader = csv.DictReader(f, keys)
         next(reader, None)
         for row in reader:
-            if row['comments'][0] != '0' or row['year'] and row['year'][0:4] == '2017':
+            if (float(row['comments']) > 5) or (row['year'] and row['year'][0:4] == '2017'):
                 gender = 'u' if row['gender'] == 'мужчин и женщин' else 'f' if row['gender'] == 'женщин' else 'm'
                 brand, _ = Brand.objects.get_or_create(title=row['designer'])
                 aroma, _ = Aroma.objects.get_or_create(title=row['title'], gender=gender, brand=brand)
-                group, _ = Group.objects.get_or_create(id=6)
+                group, _ = Group.objects.get_or_create(id=5)
+                aroma.groups.add(group)
+                group, _ = Group.objects.get_or_create(id=9)
+                aroma.groups.add(group)
+                group, _ = Group.objects.get_or_create(id=15)
                 aroma.groups.add(group)
                 aroma.pic = row['pic']
                 if row['year'] != '':
@@ -27,29 +39,17 @@ def group_import_csv(name='/Users/marinapolyakova/PycharmProjects/fragrantica_pa
                         nose, _ = Nose.objects.get_or_create(name=nose)
                         aroma.noses.add(nose)
                 if row['Ноты Аромата']:
-                    for note in ast.literal_eval(row['Ноты Аромата']):
-                        note, _ = Note.objects.get_or_create(title=note)
-                        note.save()
-                        cnote, _ = CategoryNotes.objects.get_or_create(aroma=aroma, note=note, category=CategoryNotes.GENERAL_NOTES)
-                        cnote.save()
+                    note_parser(row['Ноты Аромата'], CategoryNotes.GENERAL_NOTES)
+
                 if row['верхние Ноты']:
-                    for note in ast.literal_eval(row['верхние Ноты']):
-                        note, _ = Note.objects.get_or_create(title=note)
-                        note.save()
-                        cnote, _ = CategoryNotes.objects.get_or_create(aroma=aroma, note=note, category=CategoryNotes.TOP_NOTES)
-                        cnote.save()
+                    note_parser(row['верхние Ноты'], CategoryNotes.TOP_NOTES)
+
                 if row['средние Ноты']:
-                    for note in ast.literal_eval(row['средние Ноты']):
-                        note, _ = Note.objects.get_or_create(title=note)
-                        note.save()
-                        cnote, _ = CategoryNotes.objects.get_or_create(aroma=aroma, note=note, category=CategoryNotes.MIDDLE_NOTES)
-                        cnote.save()
+                    note_parser(row['средние Ноты'], CategoryNotes.MIDDLE_NOTES)
+
                 if row['база Ноты']:
-                    for note in ast.literal_eval(row['база Ноты']):
-                        note, _ = Note.objects.get_or_create(title=note)
-                        note.save()
-                        cnote, _ = CategoryNotes.objects.get_or_create(aroma=aroma, note=note, category=CategoryNotes.BASE_NOTES)
-                        cnote.save()
+                    note_parser(row['база Ноты'], CategoryNotes.BASE_NOTES)
+
                 aroma.save()
                 AromaCounter.objects.get_or_create(aroma=aroma, num_comments=float(row['comments']))
 
