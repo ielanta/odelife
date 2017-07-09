6de9009d8d06227998ceeea7e7f0577e7937ad33
ielanta <vanda2@mail.ru>
2017-07-09 21:03:02 +0300

    Refactor full name

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -5,10 +5,14 @@ from accounts.models import Account
 
 class UserSerializer(serializers.ModelSerializer):
     profile_url = serializers.SerializerMethodField()
+    full_name = serializers.SerializerMethodField()
     class Meta:
         model = Account
-        fields = ('id', 'get_full_name', 'profile_url')
+        fields = ('id', 'full_name', 'profile_url')
 
+    @staticmethod
+    def get_full_name(obj):
+        return obj.account.get_full_name()
 
     @staticmethod
     def get_profile_url(obj):
